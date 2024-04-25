cbf6a5cae0359236067f1440efd335f3fab49eaf
Prajval Patel <prajval1420@gmail.com>
2024-04-25 11:48:20 -0400

    common navbar js refactored

--- static/js/common.js
+++ static/js/common.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const cartBadge = document.getElementById('cart_items_badge');
+    const navbarBadge = document.getElementById('navbar_toggler_icon_badge');
+    
     //Footer JS
     const footerYear = document.getElementById('current_Year');
     footerYear.innerHTML = new Date().getFullYear();
 
     // Navbar JS
     document.getElementById('nav_login_button').addEventListener("click", () => window.location.href = '/profile');
+    document.getElementById('nav_cart_button').addEventListener("click", () => window.location.href = '/mycart');
     document.getElementById('search_input').addEventListener("keypress", (event) => {
         if (event.keyCode === 13) {
             event.preventDefault();
